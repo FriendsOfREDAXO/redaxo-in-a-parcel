0b476bb6b5c93959f0eb4d5139c649135bbb7fa8
schuer <ds@decaf.de>
2022-02-05 23:58:13 +0100

    add demo index page

---
+++ redaxo/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>test</title>
+    <link rel="stylesheet" href="./assets/styles/styles.css">
+</head>
+<body>
+
+<h1>Test</h1>
+
+<script src="./assets/scripts/scripts.js"></script>
+</body>
+</html>
